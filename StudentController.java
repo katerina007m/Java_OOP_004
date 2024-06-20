2155806c6de4e6f50794e0409a42dfc390403e6e
katerina007m <129852100+katerina007m@users.noreply.github.com>
2024-06-20 15:21:16 +0300

    Update StudentController.java
    correct

--- StudentController.java
+++ StudentController.java
@@ -6,14 +6,14 @@ public class StudentController implements UserController<Student> {
     StudentGroup students = new StudentGroup(new ArrayList<>());
     UserView<Student> userView = new UserView<Student>();
 
-    @Override
+   @Override
     public void create(String firstName, String secondName, String lastName) {
         int maxID = 0;
         if (!students.getStudentList().isEmpty()) {
             maxID = students.getStudentList().get(students.getStudentList().size() - 1).getStudentId() + 1;
-            Student student = new Student(maxID, firstName, secondName, lastName);
-            students.addStudent(student);
         }
+        Student student = new Student(maxID, firstName, secondName, lastName);
+        students.addStudent(student);
     }
 
     
